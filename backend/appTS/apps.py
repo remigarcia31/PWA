bdfbc373ca41dd6315d93d4a0dade16c6c03ca8f
remigarcia31 <remi.garcia@iut-rodez.fr>
2021-08-26 23:25:58 +0200

    PWA

--- backend/appTS/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class ApptsConfig(AppConfig):
-    name = 'appTS'
--- backend/appTS/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import AppTS
-
-class AppTSSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = AppTS
-        fields = ('id', 'title', 'description', 'completed')
